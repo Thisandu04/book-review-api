a1adc1b544d6e36c3caf657b76a2a09ce1a18ea8
Thisandu Dahanayake <thisandu2004sethmitha@gmail.com>
2026-06-09 17:29:53 +0530

    Added server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth',             require('./routes/authRoutes'));
+app.use('/api/books',            require('./routes/bookRoutes'));
+app.use('/api/books/:bookId/reviews', require('./routes/reviewRoutes'));
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
